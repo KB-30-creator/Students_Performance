0ec6f30f14046bff72489d2d5bc7f6d451e5fe9d
Karuna Bhatia <karuna.16.kb@gmail.com>
2025-09-11 02:04:15 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages,setup
+
+
+
+setup=(
+name="Student_Performance",
+version="0.0.1",
+author="Karuna Bhatia",
+author_email="karuna.16.kb@gmail.com",
+packages= find_packages(),
+install_requires=["pandas","numpy","seaborn"]
+
+
+)
